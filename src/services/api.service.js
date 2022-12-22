50e057c1578e2feb1390803c6e8d7f8432ca7644
Kelli Lilly <kelli.lilly05@gmail.com>
2022-12-22 12:38:59 -0600

    feat: :construction: WIP api service with TODO

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -2,8 +2,9 @@ const BASE_URL = "http://localhost:3001/";
 
 export default {
   // Show one random data (people, places, situations)
-  async show() {
-    const resp = await fetch("http://localhost:3001/people/1");
+  // TODO: Access any resource id from 1-3 (random).
+  async show(resource) {
+    const resp = await fetch(`${BASE_URL}/${resource}/1`);
 
     // Because this METHOD is 'async', it will return a promise (no need for await)
     // Whoever calls this method will need to use 'await' to get the data
